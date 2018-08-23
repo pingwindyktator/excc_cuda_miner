b58239bca77701f44b37d92d3b63b5f0f6e188ed
pingwindyktator <ja2222ja@gmail.com>
2018-08-23 15:50:53 +0200

    improve build script

--- eqcuda1445/build.sh
+++ eqcuda1445/build.sh
@@ -8,5 +8,5 @@ cd "$DIR" > /dev/null                                                           
 g++ $CXX_FLAGS -fPIC -c blake/blake2b.cpp -o blake.o                                    && \
 nvcc $NVCC_FLAGS -rdc=true --compiler-options '-fPIC' -c -o solver.o  solver.cu         && \
 nvcc $NVCC_FLAGS -dlink --compiler-options '-fPIC' -o  eqcuda1445.o solver.o            && \
-g++ $CXX_FLAGS -fPIC -Wl,-soname,libeqcuda1445.so -shared -o libeqcuda1445.so eqcuda1445.o solver.o blake.o -L/usr/local/cuda/lib64 -lcudart_static  -ldl -lrt -lpthread
+g++ $CXX_FLAGS -fPIC -Wl,-soname,libeqcuda1445.so -shared -o libeqcuda1445.so eqcuda1445.o solver.o blake.o -L/opt/cuda/lib64 -L/usr/local/cuda/lib64 -lcudart_static  -ldl -lrt -lpthread
 
